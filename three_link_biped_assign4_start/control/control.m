3c2b82e30ee0ca09f4f367f1ccb988569a651470
Ivan-Daniel Sievering <ivan.sievering@epfl.ch>
2019-11-19 14:50:28 +0100

    Control evolve

--- three_link_biped_assign4_start/control/control.m
+++ three_link_biped_assign4_start/control/control.m
@@ -3,6 +3,7 @@ function u = control(q,dq) %control(t, q, dq, q0, dq0, step_number)
 % function
 % you don't necessarily need to use all the inputs to this control function
 
+<<<<<<< Updated upstream
 u = zeros(2,1); 
 
 [Kp,Kd,q3_des,dq3_des] = control_hyper_parameters;
@@ -10,4 +11,9 @@ u = zeros(2,1);
 u(1) = -Kp*(q3_des-q(3))+Kd*(dq3_des - dq(3));
 disp(u(1))
 
+=======
+    [Kd, Kp] = control_hyper_parameters();
+    u = - Kd*(dq0-dq) - Kp*(q0-q);
+    
+>>>>>>> Stashed changes
 end
--- three_link_biped_assign4_start/control/control_hyper_parameters.m
+++ three_link_biped_assign4_start/control/control_hyper_parameters.m
@@ -1,5 +1,6 @@
 % You can set any hyper parameters of the control function here; you may or
 % may not want to use the step_number as the input of the function. 
+<<<<<<< Updated upstream
 function [Kd,Kp,q3_des, dq3_des] = control_hyper_parameters() %step_number)
 
 Kd = 0; 
@@ -7,5 +8,10 @@ Kp = 1;
 
 q3_des = 0;
 dq3_des = 0.1;
+=======
+function [Kd,Kp] = control_hyper_parameters()
+>>>>>>> Stashed changes
 
+    Kd = 0.1;
+    Kp = 5;
 end
