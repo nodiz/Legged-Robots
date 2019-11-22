236feac472843d6663d74261941fde70540be65f
Ivan-Daniel Sievering <ivan.sievering@epfl.ch>
2019-11-22 15:57:44 +0100

    Control

--- three_link_biped_assign4_start/control/control.m
+++ three_link_biped_assign4_start/control/control.m
@@ -3,17 +3,12 @@ function u = control(q,dq) %control(t, q, dq, q0, dq0, step_number)
 % function
 % you don't necessarily need to use all the inputs to this control function
 
-<<<<<<< Updated upstream
+
 u = zeros(2,1); 
 
-[Kp,Kd,q3_des,dq3_des] = control_hyper_parameters;
+[Kp,Kd, q1_des, dq1_des, q2_des, dq2_des, q3_des,dq3_des] = control_hyper_parameters(q,dq,q0,dq0);
 
-u(1) = -Kp*(q3_des-q(3))+Kd*(dq3_des - dq(3));
-disp(u(1))
+u(1) = Kp(1)*(q1_des - q(1) - q3_des + q(3)) + Kd(2)*(dq1_des - dq(1) - dq3_des + dq(3));
+u(2) = Kp(2)*(q2_des - q(2) - q3_des + q(3)) + Kd(2)*(dq2_des - dq(2) - dq3_des + dq(3));
 
-=======
-    [Kd, Kp] = control_hyper_parameters();
-    u = - Kd*(dq0-dq) - Kp*(q0-q);
-    
->>>>>>> Stashed changes
 end
--- three_link_biped_assign4_start/control/control_hyper_parameters.m
+++ three_link_biped_assign4_start/control/control_hyper_parameters.m
@@ -1,17 +1,21 @@
 % You can set any hyper parameters of the control function here; you may or
 % may not want to use the step_number as the input of the function. 
-<<<<<<< Updated upstream
-function [Kd,Kp,q3_des, dq3_des] = control_hyper_parameters() %step_number)
 
-Kd = 0; 
-Kp = 1;
+function [Kd,Kp,q1_des,dq1_des,q2_des, dq2_des,q3_des, dq3_des] = control_hyper_parameters(q,dq,q0,dq0) %step_number)
 
-q3_des = 0;
-dq3_des = 0.1;
-=======
-function [Kd,Kp] = control_hyper_parameters()
->>>>>>> Stashed changes
+Kd = zeros(1,2);
+Kp = zeros(1,2);
 
-    Kd = 0.1;
-    Kp = 5;
+Kd(1) = 0.1;
+Kd(2) = 0.1;
+Kp(1) = 50;
+Kp(2) = 50;
+
+q1_des = -pi/6;
+dq1_des = 0;
+q2_des = pi/6;
+dq2_des = 0;
+q3_des = pi/13;
+dq3_des = 0;
+
 end
